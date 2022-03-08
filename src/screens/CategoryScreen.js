3bba6ba2a956c59975a27c57989199389845efb8
Harikarthyk <hari.jsmith494@gmail.com>
2022-03-08 08:38:07 +0530

    Category Screen Completed .

--- src/screens/CategoryScreen.js
+++ src/screens/CategoryScreen.js
@@ -1,13 +1,79 @@
-import React from 'react'
-import { SafeAreaView, StyleSheet } from 'react-native'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { 
+    FlatList, 
+    SafeAreaView, 
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+import normalize from 'react-native-normalize';
+import AudioView from '../components/AudioView';
 import colors from '../theme/colors';
+import fonts from '../theme/fonts';
 
-function CategoryScreen() {
+const Heading = ({content}) => {
+    return(
+        <Text
+            style={{
+                fontSize: fonts.size.font20,
+                color: colors.primary,
+                fontWeight: fonts.weight.semi,
+                marginTop: normalize(20),
+                marginBottom: normalize(12)
+            }}
+        >
+            {content}
+        </Text>
+    )
+}
+
+function CategoryScreen({navigation, route}) {
+    const { category } = route.params;
+
+    const [categoryInfo, setCategoryInfo] = useState({
+        isLoading: true,
+        audios: []
+    })
+
+    const getCategoryInfo = async() => {
+        try{
+            const { data } = await axios.get(`https://raw.githubusercontent.com/Harikarthyk/Medify-Content/main/${category.name}.json`);
+            setCategoryInfo({
+                isLoading: false,
+                audioTracks: data?.audios
+            });
+        }catch(error){
+            console.log(error)
+            setCategoryInfo({
+                isLoading: false,
+                audioTracks: []
+            });
+        }
+    }
+
+    useEffect(() => {
+        getCategoryInfo();
+    }, []);
+
     return (
         <SafeAreaView
             style={styles.container}
         >
 
+            <Heading  content={`Medify's ${category?.name}`} />
+            <View style={styles.bottomBorder} />
+
+
+            <FlatList
+                style={{
+                    flex: 1
+                }}
+                data={categoryInfo.audioTracks}
+                renderItem={({item}) => <AudioView item={item} navigation={navigation} />}
+                keyExtractor={(item) => item.id}
+
+            />
         </SafeAreaView>
     )
 }
@@ -15,7 +81,14 @@ function CategoryScreen() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: colors.backgroundColor
+        backgroundColor: colors.backgroundColor,
+        padding: normalize(15)
+    },
+    bottomBorder: {
+        height: normalize(4), 
+        backgroundColor: colors.secondary, 
+        width: normalize(120), 
+        marginBottom: normalize(20)
     }
 })
 
