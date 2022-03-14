8710103b5a42aafc236ea6cca34275d2282c1893
Harikarthyk <hari.jsmith494@gmail.com>
2022-03-14 16:54:18 +0530

    DailyQuotes Screen Added .

--- src/screens/DailyQuotesScreen.js
+++ src/screens/DailyQuotesScreen.js
@@ -5,14 +5,71 @@ import {
     ScrollView, 
     StyleSheet, 
     View,
-    Text
+    Text,
+    useColorScheme,
+    FlatList
 } from 'react-native';
+import FastImage from 'react-native-fast-image';
 import normalize from 'react-native-normalize';
+import colors from '../theme/colors';
+import fonts from '../theme/fonts';
 import metrics from '../theme/metrics';
 
 const QUOTES_URL = `https://quotes.rest/qod?language=en`;
 
+
+const Heading = () => {
+    return(
+        <Text
+            style={{
+                fontSize: fonts.size.font20,
+                color: colors.primary,
+                fontWeight: fonts.weight.semi,
+                marginTop: normalize(20),
+                marginBottom: normalize(12)
+            }}
+        >
+            " Quotes of the Day "
+        </Text>
+    )
+}
+
+const displayTags = (tags, theme) => {
+    return (
+        <FlatList
+            contentContainerStyle={{ 
+                flexDirection: 'row',
+                flexWrap: 'wrap',
+                maxWidth: '100%'
+            }}
+           
+            data={Object.entries(tags)}
+            renderItem={({item}) => {
+                return (
+                    <Text
+                        style={{
+                            padding: normalize(14),
+                            backgroundColor: theme === 'dark' ? colors.black : colors.white,
+                            margin: normalize(5),
+                            borderRadius: normalize(5),
+                            textTransform: 'capitalize',
+                            elevation: 2
+                        }}
+                    >
+                        {item[1]}
+                    </Text>
+                )
+            }}
+            
+            keyExtractor={(item) => item[1]}
+        />
+    )
+}
+
+
 function DailyQuotesScreen({ navigation }) {
+
+    const theme = useColorScheme();
 
     const [quotes, setQuotes] = useState({
         isLoading: true,
@@ -65,26 +122,44 @@ function DailyQuotesScreen({ navigation }) {
         <SafeAreaView
             style={styles.container}
         >
+
+            <Heading  />
+            <View style={styles.bottomBorder}/>
+
             <ScrollView
                 style={styles.quotes}
-                showsHorizontalScrollIndicator={false}
+                showsVerticalScrollIndicator={false}
             >
 
                 <View
-                    style={styles.quotesTextContainer}
+                    style={[styles.quotesContainer, { backgroundColor: theme === 'dark' ? colors.cardColorDark : colors.cardColorDefault }]}
                 >
+                    <FastImage
+                        source={{
+                            uri: quotes.content?.background
+                        }}
+                        style={styles.background}
+
+                    />  
                     <Text
                         style={styles.quotesText}
                     >
-
+                        {quotes.content?.quote}
                     </Text>
+                    <Text
+                        style={styles.author}
+                    >
+                       - {quotes.content?.author}
+                    </Text>
+                    <View
+                        style={{
+                            flex: 1
+                        }}
+                    >
+                        {quotes.content?.tags && displayTags(quotes.content?.tags, theme)}
+                    </View>
                 </View>
-                <Text
-                    style={styles.author}
-                >
-
-                </Text>
-
+                        
             </ScrollView>
 
         </SafeAreaView>
@@ -93,10 +168,44 @@ function DailyQuotesScreen({ navigation }) {
 
 const styles = StyleSheet.create({
     container:{
-        flex: 1
+        flex: 1,
+        padding: normalize(15)
     },
     quotes: {
-        height: normalize (200)
+        flex: 1
+    },
+    bottomBorder: {
+        height: normalize(4), 
+        backgroundColor: colors.secondary, 
+        width: normalize(140), 
+        marginBottom: normalize(20)
+    },
+    quotesContainer:{
+        padding: normalize(20),
+        elevation: 2,
+        borderRadius: normalize(12),
+        width: '100%'
+    },
+    quotesText: {
+        fontFamily: fonts.type.montserratRegular,
+        fontWeight: fonts.weight.normal,
+        fontSize: fonts.size.font16,
+        marginVertical: normalize(20)
+    },
+    author:{
+        fontFamily: fonts.type.montserratBold,
+        fontWeight: fonts.weight.semi,
+        fontSize: fonts.size.font14,
+        color: colors.secondary,
+        marginVertical: normalize(10),
+        textAlign: 'right'
+    },
+    background:{
+        colors: colors.textGrey,
+        width: '100%',
+        height: normalize(220),
+        borderRadius: normalize(12),
+        elevation: 2
     }
 })
 
