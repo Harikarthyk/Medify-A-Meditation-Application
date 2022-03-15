564f6dff704ca87f08788ccccd6e125ac7c94ac4
Harikarthyk <hkarthikeyyan@audyglobal.in>
2022-03-15 14:33:04 +0530

    package name changed .

--- ios/MeditateTests/MeditateTests.m
+++ ios/MeditateTests/MeditateTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface medifyTests : XCTestCase
+@interface MeditateTests : XCTestCase
 
 @end
 
-@implementation medifyTests
+@implementation MeditateTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
